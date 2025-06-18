13ab957473c75c20181c074b2e79aaf5ae2716e5
Techie Pugal <pugalpugalee333@gmail.com>
2025-06-18 12:54:25 +0530

    chnage crdntl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSHZAKsRFrHer7lno6Rknr3w5j-xVglVc",
-  authDomain: "employeemanagement-16dba.firebaseapp.com",
-  projectId: "employeemanagement-16dba",
-  storageBucket: "employeemanagement-16dba.firebasestorage.app",
-  messagingSenderId: "723276151197",
-  appId: "1:723276151197:web:5df89d54869fc42dd4d774",
-  measurementId: "G-BT1YLST16T"
+  apiKey: "AIzaSyDicQksIDusUdMK7k2fIt2cvxyCY8yZg3c",
+  authDomain: "erpv02.firebaseapp.com",
+  projectId: "erpv02",
+  storageBucket: "erpv02.firebasestorage.app",
+  messagingSenderId: "453487579197",
+  appId: "1:453487579197:web:255e8fb9745dc61e7c9a54",
+  measurementId: "G-VP7GPT0J7J"
 };
 
 const app = initializeApp(firebaseConfig);
