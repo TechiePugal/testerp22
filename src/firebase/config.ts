75d8ce8cbcce05ba531e8e2e3b844b4a1f7b777f
KannanAmirthalingam <mhr.otpauthentication@gmail.com>
2025-07-08 16:20:12 +0530

    Updated config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDicQksIDusUdMK7k2fIt2cvxyCY8yZg3c',
-  authDomain: 'erpv02.firebaseapp.com',
-  projectId: 'erpv02',
-  storageBucket: 'erpv02.appspot.com',
-  messagingSenderId: '453487579197',
-  appId: '1:453487579197:web:255e8fb9745dc61e7c9a54',
-  measurementId: 'G-VP7GPT0J7J',
+  apiKey: "AIzaSyDSHZAKsRFrHer7lno6Rknr3w5j-xVglVc",
+  authDomain: "employeemanagement-16dba.firebaseapp.com",
+  projectId: "employeemanagement-16dba",
+  storageBucket: "employeemanagement-16dba.firebasestorage.app",
+  messagingSenderId: "723276151197",
+  appId: "1:723276151197:web:5df89d54869fc42dd4d774",
+  measurementId: "G-BT1YLST16T"
 };
 
 const app = initializeApp(firebaseConfig);
